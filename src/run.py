ea315417a4ddca740884aac5a861337e20b1412d
manish <ms3236@cornell.edu>
2019-11-21 17:44:49 -0500

    Update requirements.txt

--- src/run.py
+++ src/run.py
@@ -2,7 +2,4 @@ from app import app
 from os import environ
 
 if __name__ == "__main__":
-    HOST = "0.0.0.0"
-    PORT = environ["PORT"]
-    print("Server running on {}:{}".format(HOST, PORT))
-    app.run(host="localhost", port=PORT)
+    app.run(host="0.0.0.0", port=environ["PORT"])
