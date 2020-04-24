f1c7f1cdd738a61f3abd38bee7acfd28cbce7f5f
yuna878 <yuna878@gmail.com>
2020-04-24 16:39:03 +0900

    Clear section tracking history when new semester starts

--- src/app/coursegrab/dao/__init__.py
+++ src/app/coursegrab/dao/__init__.py
@@ -4,4 +4,4 @@ from app import db
 from app.coursegrab.models.course import Course
 from app.coursegrab.models.section import Section
 from app.coursegrab.models.semester import Semester
-from app.coursegrab.models.user import User
+from app.coursegrab.models.user import User, users_to_sections
