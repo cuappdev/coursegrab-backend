d9d9bb0f2b4e9867a059f6d62f8e10d92c5ad2d7
meganle <meganle7@gmail.com>
2020-07-22 14:00:56 -0700

    Allow coursegrabappstore@gmail.com to register

--- src/app/coursegrab/controllers/initialize_session_controller.py
+++ src/app/coursegrab/controllers/initialize_session_controller.py
@@ -25,7 +25,7 @@ class InitializeSessionController(AppDevController):
 
             # ID token is valid. Get the user's Google Account information.
             email, first_name, last_name = id_info["email"], id_info["given_name"], id_info["family_name"]
-            if email[email.find("@") + 1 :] != "cornell.edu":
+            if email != "coursegrabappstore@gmail.com" and email[email.find("@") + 1 :] != "cornell.edu":
                 raise Exception("You must use a Cornell email")
 
             created, user = users_dao.create_user(email, first_name, last_name)
