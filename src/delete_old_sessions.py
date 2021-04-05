348d34f72d8fefc4b5bb6996e7c2e0a38293816e
Kate <ksl67@cornell.edu>
2021-04-04 20:04:54 -0500

    Fix script bugs and add migration

--- src/delete_old_sessions.py
+++ src/delete_old_sessions.py
@@ -3,22 +3,22 @@ from app import app, db
 from app.coursegrab.models.session import Session
 import datetime
 
+# Deletes user sessions that have been last used 10 weeks or more ago
 def delete_old_sessions():
-  sessions = [s.serialize_session() for s in Session.query.all()]
+  sessions = Session.query.all()
   outdated_sessions = []
   for s in sessions:
-    if s["session_expiration"] < datetime.datetime.now() or s["last_used"] > (datetime.datetime.now() - datetime.timedelta(weeks=10)): 
+    if s.session_expiration < datetime.datetime.now() or s.last_used <= (datetime.datetime.now() - datetime.timedelta(weeks=10)): 
       db.session.delete(s)
-      outdated_sessions.append(s)
+      outdated_sessions.append(s.serialize_session())
   db.session.commit()
   return outdated_sessions
 
+# Test delete old sessions conditions
 def test():
-  new_session = db.Session(device_token="test", device_type="PHONE", user_id=1)
+  new_session = Session(device_token="test", device_type="PHONE", user_id=1)
   db.session.add(new_session)
-  db.session.commit()
   assert delete_old_sessions() == []
-  new_session["last_used"] = datetime.datetime.now() - datetime.timedelta(weeks=11)
+  db.session.commit()
+  new_session.last_used = datetime.datetime.now() - datetime.timedelta(weeks=11)
   assert delete_old_sessions() == [new_session.serialize_session()]
-
-# delete_old_sessions()
