36cef9dd55f49869c5306ec31ab461fb44ff6fee
Kate <ksl67@cornell.edu>
2021-04-04 20:30:56 -0500

    Add more test

--- src/delete_old_sessions.py
+++ src/delete_old_sessions.py
@@ -8,17 +8,33 @@ def delete_old_sessions():
   sessions = Session.query.all()
   outdated_sessions = []
   for s in sessions:
-    if s.session_expiration < datetime.datetime.now() or s.last_used <= (datetime.datetime.now() - datetime.timedelta(weeks=10)): 
+    if s.session_expiration <= datetime.datetime.now() or s.last_used <= (datetime.datetime.now() - datetime.timedelta(weeks=10)): 
       db.session.delete(s)
       outdated_sessions.append(s.serialize_session())
   db.session.commit()
   return outdated_sessions
 
-# Test delete old sessions conditions
+# Test delete old sessions function
 def test():
   new_session = Session(device_token="test", device_type="PHONE", user_id=1)
   db.session.add(new_session)
   assert delete_old_sessions() == []
-  db.session.commit()
+
   new_session.last_used = datetime.datetime.now() - datetime.timedelta(weeks=11)
   assert delete_old_sessions() == [new_session.serialize_session()]
+
+  expired_session = Session(device_token="test3", device_type="PHONE", user_id=1)
+  expired_session.session_expiration = datetime.datetime.now() - datetime.timedelta(weeks=1)
+  db.session.add(expired_session)
+  assert delete_old_sessions() == [expired_session.serialize_session()]
+
+  new_expired_session = Session(device_token="test4", device_type="PHONE", user_id=1)
+  new_expired_session.session_expiration = datetime.datetime.now()
+  db.session.add(new_expired_session)
+  assert delete_old_sessions() == [new_expired_session.serialize_session()]
+
+  last_session = Session(device_token="test5", device_type="PHONE", user_id=1)
+  db.session.add(last_session)
+  last_session.last_used = datetime.datetime.now() - datetime.timedelta(weeks=10)
+  assert delete_old_sessions() == [last_session.serialize_session()]
+
