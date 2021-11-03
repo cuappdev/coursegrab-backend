131481c52834dbba8329f1e08fd65c76ba3517fd
Kate Liang <41009676+katesliang@users.noreply.github.com>
2021-11-03 16:50:54 -0500

    Fix docker build error (#101)

--- src/app/coursegrab/dao/courses_dao.py
+++ src/app/coursegrab/dao/courses_dao.py
@@ -5,6 +5,7 @@ from ..utils.constants import NUM_SEARCH_RESULT
 def get_course_by_id(course_id):
     return Course.query.filter_by(id=course_id).first()
 
+
 def get_course_by_subject_and_course_num(subject_code, course_num):
     return Course.query.filter_by(subject_code=subject_code, course_num=course_num).first()
 
@@ -26,11 +27,13 @@ def search_courses(query):
     results = sorted(results, key = lambda r : find_query_index(r, query))
     return results
 
+
 def find_query_index(course, key):
     try: 
         return course.search_string.lower().index(key.lower()) 
     except(ValueError): 
         return -1
+
 
 def clear_table():
     Course.query.delete()
